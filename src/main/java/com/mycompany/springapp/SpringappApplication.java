54a0d07830ecb208d70f90006516dbead8f77ea0
Pabitra Panda <pk.pabitra18@gmail.com>
2021-07-10 13:42:28 +0530

    Spring Scheduler

--- src/main/java/com/mycompany/springapp/SpringappApplication.java
+++ src/main/java/com/mycompany/springapp/SpringappApplication.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringappApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringappApplication.class);
